f5e211fe59e447fe7fe7a9e2567370f57bda4054
MD Emon Biswas <148654125+emon-06@users.noreply.github.com>
2025-05-30 16:50:31 +0600

    Create 1266B.cpp

---
+++ 1266B.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        long long x;
+        cin >> x;
+        if (x % 14 >= 1 && x % 14 <= 6 && x >= 15)
+            cout << "YES\n";
+        else
+            cout << "NO\n";
+    }
+    return 0;
+}
