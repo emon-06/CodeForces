5264a4953f4c1847de60c64a72dc02ef04e11b6b
MD Emon Biswas <148654125+emon-06@users.noreply.github.com>
2025-07-12 23:08:51 +0600

    Create 1703A.cpp

---
+++ 1703A.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string s;
+        cin >> s;
+        transform(s.begin(), s.end(), s.begin(), ::tolower);
+        if (s == "yes") cout << "YES\n";
+        else cout << "NO\n";
+    }
+    return 0;
+}
+
+
+\
