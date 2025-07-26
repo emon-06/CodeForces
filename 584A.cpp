9b1697aba5e1429e7d3e0e985dcfd5ca2b2c6e0f
MD Emon Biswas <148654125+emon-06@users.noreply.github.com>
2025-07-26 13:26:38 +0600

    Create 584A.cpp

---
+++ 584A.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, t;
+    cin >> n >> t;
+
+    if (t == 10) {
+        if (n == 1) {
+            cout << -1 << endl;
+        } else {
+            cout << 1;
+            for (int i = 1; i < n; ++i) cout << 0;
+            cout << endl;
+        }
+    } else {
+        cout << t;
+        for (int i = 1; i < n; ++i) cout << 0;
+        cout << endl;
+    }
+
+    return 0;
+}
